6c3716f46e465aa50151fdd1bfeae8fb82a8f497
stl88083365 <37022732+stl88083365@users.noreply.github.com>
2025-06-29 17:44:50 +0800

    "FixMakeluckyPackage"

--- makelucky.sh
+++ makelucky.sh
@@ -5,10 +5,10 @@ BASE_DIR="./lucky" # Changed to relative path
 TMP_DIR="/tmp/${PLUGIN_NAME}_${RANDOM}"
 VERSION="$(date +'%Y.%m.%d')"
 
-mkdir -p "$TMP_DIR/$VERSION"
+mkdir -p "$TMP_DIR/$VERSION/lucky"
 cd "$TMP_DIR/$VERSION" || exit 1
-cp -Rf "$BASE_DIR/"* "$TMP_DIR/$VERSION/"
-chmod +x "$TMP_DIR/$VERSION/etc/lucky/lucky"
+cp -Rf "$BASE_DIR/"* "$TMP_DIR/$VERSION/lucky/"
+chmod +x "$TMP_DIR/$VERSION/lucky/etc/lucky/lucky"
 mkdir "$TMP_DIR/$VERSION/install"
 tee "$TMP_DIR/$VERSION/install/slack-desc" <<EOF
        |-----lucky------------------------------------------------------|
@@ -22,7 +22,7 @@ EOF
 
 # Create .tar.xz archive (renamed to .txz)
 # Change directory to the temporary build directory to correctly tar its contents
-(cd "$TMP_DIR/$VERSION" && tar -cvf "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar" .)
+(cd "$TMP_DIR/$VERSION" && tar -cvf "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar" lucky)
 xz -z "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar"
 mv "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar.xz" "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz"
 
