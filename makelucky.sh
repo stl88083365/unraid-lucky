30de5756a4d0795fccbddc74376b349b6700220b
stl88083365 <37022732+stl88083365@users.noreply.github.com>
2025-06-29 15:23:53 +0800

    "FixWorkflow"

--- makelucky.sh
+++ makelucky.sh
@@ -20,8 +20,13 @@ $PLUGIN_NAME: Custom $PLUGIN_NAME package for Unraid by stl88083365
 $PLUGIN_NAME:
 EOF
 
-makepkg -l n -c n "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz"
+# Create .tar.xz archive (renamed to .txz)
+# Change directory to the temporary build directory to correctly tar its contents
+(cd "$TMP_DIR/$VERSION" && tar -cvf "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar" .)
+xz -z "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar"
+mv "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.tar.xz" "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz"
+
 md5sum "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz" | awk '{print $1}' > "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz.md5"
-cp "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz" "." # Changed output directory to current
-cp "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz.md5" "." # Changed output directory to current
+cp "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz" "."
+cp "$TMP_DIR/unraid-$PLUGIN_NAME-$VERSION.txz.md5" "."
 rm -rf "$TMP_DIR"
